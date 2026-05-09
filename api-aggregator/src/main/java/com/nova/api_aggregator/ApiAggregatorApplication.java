09d798ac0056f7a4719d69416f2c9f32646fac96
Thiago Viccichi <viccichithiago@gmail.com>
2026-05-09 15:55:37 -0300

    Estructuras del Proyecto

---
+++ api-aggregator/src/main/java/com/nova/api_aggregator/ApiAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.nova.api_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAggregatorApplication.class, args);
+	}
+
+}
---
+++ api-aggregator/src/test/java/com/nova/api_aggregator/ApiAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nova.api_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
