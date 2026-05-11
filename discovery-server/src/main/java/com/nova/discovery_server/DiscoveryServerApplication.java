8bfe5e8f425941b53fdddb0ba2ae549cc1e719c8
Thiago Viccichi <viccichithiago@gmail.com>
2026-05-11 14:04:07 -0300

    Add Eureka client support and configuration for catalog, discovery, and user services

--- discovery-server/src/main/java/com/nova/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/nova/discovery_server/DiscoveryServerApplication.java
@@ -2,7 +2,8 @@ package com.nova.discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
--- user-service/src/main/java/com/nova/user_service/Application.java
+++ user-service/src/main/java/com/nova/user_service/Application.java
@@ -2,7 +2,8 @@ package com.nova.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
